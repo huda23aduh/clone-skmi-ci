d24af77084e7c55e449b77d9e57a686c9254c755
huda23aduh <huda23aduh@gmail.com>
2025-10-29 20:05:34 +0700

    fix: error when preview pdf caused by format helper

--- app/Helpers/format_helper.php
+++ app/Helpers/format_helper.php
@@ -1,7 +1,7 @@
 <?php
 // In app/Helpers/format_helper.php
 if (!function_exists('format_file_size')) {
-    function format_file_size($bytes) {
+    function format_file_size($bytes, $decimals = 2) {
         if ($bytes == 0) return '0 Bytes';
         
         $size = array('Bytes','KB','MB','GB','TB','PB','EB','ZB','YB');
