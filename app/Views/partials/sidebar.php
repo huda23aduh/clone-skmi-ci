ecf7e5c4c222420d2526a1ae229e49485ec21504
huda23aduh <huda23aduh@gmail.com>
2025-11-17 17:36:15 +0700

    fix: logo route and size

--- app/Views/partials/sidebar.php
+++ app/Views/partials/sidebar.php
@@ -3,7 +3,7 @@
     <!--begin::Sidebar Brand-->
     <div class="sidebar-brand">
       <!--begin::Brand Link-->
-      <a href="./index.html" class="brand-link">
+      <a href="/" class="brand-link">
         <!--begin::Brand Image-->
         <img
           src="<?= base_url('adminlte/assets/img/AdminLTELogo.png') ?>"
